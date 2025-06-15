497b484b631f827a45511e6ba29997aee4c6249c
Bagus Abdul Kurniawan <bagus.ak95@gmail.com>
2025-06-15 16:47:13 +0700

    fix: add validate rate limit

--- src/utils/limiter/limiter.ts
+++ src/utils/limiter/limiter.ts
@@ -5,4 +5,7 @@ export const rateLimiter = rateLimit({
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	validate: {
+		xForwardedForHeader: false,
+	},
 })
